395bcd9144ddf0a2b22e0c54c2c4246575d40d57
José García <gran.pepe@gmail.com>
2022-08-27 11:57:47 +0200

    ordenando k min

--- 1337.TheKWeakestRowsinaMatrix.c
+++ 1337.TheKWeakestRowsinaMatrix.c
@@ -42,6 +42,13 @@ int *add_row(int **mat, int *solution, int rows, int cols)
 	return (solution);
 }
 
+void	get_k_mins(int *sum_row, int *solution)
+{
+	(void)sum_row;
+	(void)solution;
+	return ;
+}
+
 int* kWeakestRows(int** mat, int matSize, int* matColSize, int k, int* returnSize)
 {
 	//mat = matriz de numeros
@@ -55,10 +62,13 @@ int* kWeakestRows(int** mat, int matSize, int* matColSize, int k, int* returnSiz
 	(void)returnSize;
 
 	int *sum_row;
+	int *solution;
 
 	sum_row = malloc(sizeof(int) * matSize);
 	add_row(mat, sum_row, matSize, *matColSize);
 	*returnSize = k;
+	solution = malloc(sizeof(int) * k);
+	get_k_mins(sum_row, solution);
 
 
 	return (sum_row);
@@ -76,7 +86,7 @@ int main(void)
 	};
 	int mat_rows = 5;
 	int mat_cols = 5;
-	kWeakestRows(mat, 5, &mat_rows, 3, &mat_cols);
+	kWeakestRows(mat, 5, &mat_rows, 5, &mat_cols);
 	print_matrix(mat, mat_rows, mat_cols);
 	return 0;
 }
