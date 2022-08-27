df60699ed48a1707221964a7f88173f87d86c87c
PepeGF <gran.pepe@gmail.com>
2022-08-27 09:49:55 +0200

    no se x donde voy

--- 1337.TheKWeakestRowsinaMatrix.c
+++ 1337.TheKWeakestRowsinaMatrix.c
@@ -1,7 +1,9 @@
+// https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
+
 #include <stdlib.h>
 #include <stdio.h>
 
-int *sum_row(int **mat, int **solution, int rows, int cols)
+int *sum_row(int **mat, int *solution, int rows, int cols)
 {
     int i;
     int j;
@@ -12,7 +14,7 @@ int *sum_row(int **mat, int **solution, int rows, int cols)
     {
         j = 0;
         row_sum = 0;
-        while (j < cols)
+        while (j < cols  && mat[i][j] == 1)
         {
             row_sum *= mat[i][j];
             j++;
@@ -20,7 +22,7 @@ int *sum_row(int **mat, int **solution, int rows, int cols)
         solution[i] = row_sum;
         i++;
     }
-    return (*solution);
+    return (solution);
 }
 
 int* kWeakestRows(int** mat, int matSize, int* matColSize, int k, int* returnSize)
@@ -29,12 +31,26 @@ int* kWeakestRows(int** mat, int matSize, int* matColSize, int k, int* returnSiz
     //matSize = numero filas
     // matColSize = numero columnas
     //k = cantidad maxima de numeros del array solucion
+    (void) mat;
+    (void) matSize;
+    (void) matColSize;
+    (void) returnSize;
 
     int *solution;
 
     solution = malloc(sizeof(int) * k);
     *returnSize = k;
 
-    
+    return NULL;
 }
 
+int main(void)
+{
+    int mat[5][5] = {{1,1,0,0,0},{1,1,1,1,0},{1,0,0,0,0},{1,1,0,0,0},{1,1,1,1,1}};
+    int **xx;
+    xx = mat;
+    int i = 0;
+
+    kWeakestRows(xx, 5, &i, 3, &i);
+    return 0;
+}
